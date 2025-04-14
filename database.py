de6d7d7ba35e3ae0d209855658fc2c58850f0173
CBeatrixBacolanta <bacolanta.celeste173@gmail.com>
2025-04-14 22:22:07 +0800

    second commit

--- database.py
+++ database.py
@@ -1,16 +1,15 @@
-import os 
-from sqlalchemy import create_engine 
+import os
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 # Using environment variable for database URL 
-database_url = os.environ.get("DATABASE_URL") 
+database_url = os.environ.get("DATABASE_URL")
 
 # Creating the engine to interact with PostgreSQL 
-engine = create_engine(database_url, echo=True) 
+engine = create_engine(database_url, echo=True)
 
 # Creating a session local class to interact with the database 
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, 
-bind=engine) 
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 # Base class to create tables 
-Base = declarative_base() 
+Base = declarative_base()
--- models.py
+++ models.py
@@ -1,9 +1,9 @@
-from sqlalchemy import Column, Integer, String 
-from database import Base 
+from sqlalchemy import Column, Integer, String
+from database import Base
 
-class Book(Base): 
-    __tablename__ = "books" 
+class Book(Base):
+    __tablename__ = "books"
     
-    id = Column(Integer, primary_key=True, index=True) 
-    title = Column(String, index=True) 
-    author = Column(String) 
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True)
+    author = Column(String)
